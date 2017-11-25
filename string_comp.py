e67e4302954e63b2835212cc83634b3e3d5b35af
Anastasia <iakunina.anastasia@yandex.ru>
2017-11-25 13:15:15 +0300

    ...

---
+++ string_comp.py
@@ -0,0 +1,26 @@
+str1=12345678
+str2='123456'
+
+def string_comp (string_1,string_2):
+    string_1=str(string_1)
+    string_2=str(string_2)
+    print(string_1)
+    print(type(string_1))
+    print(string_2)
+    print(type(string_2))
+    try:
+        if string_1 == string_2:
+            return 1
+        else:
+            if len(string_1) > len(string_2):
+                return 2
+            elif string_2 =='learn':
+                return 3
+    except (TypeError):
+        print('Неверный тип данных')
+
+if __name__ == '__main__':
+    val=string_comp(str1,str2)
+    print(val)
+
+
